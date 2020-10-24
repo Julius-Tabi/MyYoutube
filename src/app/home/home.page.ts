e5c2859de290f509e398b188af6ffc4643dc9840
Julius-Tabi <67975193+Julius-Tabi@users.noreply.github.com>
2020-10-24 09:40:40 +0200

    almost

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { data } from 'jquery';
+// import { data } from 'jquery';
+import {YoutubeService} from '../services/youtube.service'
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  channels: any;
+  @ViewChild('channelName') channelName: ElementRef;
+  constructor(private youtube: YoutubeService) { }
+  
+  ngOnInit() {
+    this.youtube.getChannels("programming").subscribe((data) => {
+      console.log(data);
+      this.channels = data.items;
+    })
+  }
+  
+  getData() {
+    var channelName = this.channelName.nativeElement.value;
+     this.youtube.getChannels(channelName).subscribe((data) => {
+      console.log(data);
+      this.channels = data.items;
+    })
+  }
 
 }
